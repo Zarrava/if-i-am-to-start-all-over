ad0a43536240b9d6e408e20f9f266a03f6799fb4
Primordialt <113912642+Primordialt@users.noreply.github.com>
2025-09-28 09:36:52 +0100

    my update

--- hello_world.py
+++ hello_world.py
@@ -2,8 +2,10 @@ print ("Hello Python World!")
 # It is widely believed that writing this first line of code brings you goodluck on your journey.
 
 
-#next line is to add some variables to the above code
+#next line is to add some variables to the above code.
 
 message = "Hello Python Worlda!"
+yourName = "Elliott"
 
 print(message)
+print(yourName)
