ff702ce98751a922fc0e877766589027599b5511
Primordialt <113912642+Primordialt@users.noreply.github.com>
2025-09-28 09:25:07 +0100

    initial commit

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,9 @@
 print ("Hello Python World!")
 # It is widely believed that writing this first line of code brings you goodluck on your journey.
+
+
+#next line is to add some variables to the above code
+
+message = "Hello Python Worlda!"
+
+print(message)
