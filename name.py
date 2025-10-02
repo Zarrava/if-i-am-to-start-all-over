cf8915306f24e71d947ad690742df5ba656b5f10
Primordialt <113912642+Primordialt@users.noreply.github.com>
2025-10-02 11:39:55 +0100

    my update

---
+++ name.py
@@ -0,0 +1,8 @@
+name = "elliott akindele"
+
+print(name.title()) #this makes first letter of each word Uppercase.
+print(name.upper()) #this returns the variable on Uppercase letters.
+print(name.lower()) #this returns the variable on Lowercase letters.
+
+#if you notice, there is a .title() after the name variable
+#this is called a Method, it the ability to make python act on a data.
