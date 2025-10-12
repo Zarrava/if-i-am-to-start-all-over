d0286f934d3bc83fc9ad3a487f375e11ce9dbc05
Primordialt <113912642+Primordialt@users.noreply.github.com>
2025-10-12 17:27:59 +0100

    Update exercise_9_solution.py

--- Solutions/exercise_9_solution.py
+++ Solutions/exercise_9_solution.py
@@ -1,2 +1,2 @@
-username = "Elliott"
-print(f"Hello {username.title()}, would you like to learn some Python today?")
+username = "Elliott" #stores data into the username
+print(f"Hello {username.title()}, would you like to learn some Python today?") #returns to the screen with a title case formatted version and some additional information.
