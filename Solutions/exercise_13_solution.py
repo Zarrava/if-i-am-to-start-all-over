ba08c70268201c1c4c5006b7f79aec4ca339e77e
Primordialt <113912642+Primordialt@users.noreply.github.com>
2025-10-14 12:41:22 +0100

    solution

--- Solutions/exercise_13_solution.py
+++ Solutions/exercise_13_solution.py
@@ -1,8 +1,8 @@
-person_name = "|\t \n elliott     |" #input stored in a variable/
+person_name = "   \t elliott   \n  " #input stored in a variable/
 
-print (f"This is the raw format: {person_name} \n") #variable displayed to the screen in raw format
+print (f"This is the raw format: |{person_name}| \n") #variable displayed to the screen in raw format
 
 #variable displays to screen in left, right and full strip respectively
-print (f"This is the Left stripped version: {person_name.title().lstrip()}\n")
-print (f"This is the right stripped version: {person_name.title().rstrip()}\n")
-print (f"This is the Full stripped version: {person_name.title().strip()}\n")
+print (f"This is the Left stripped version: |{person_name.title().lstrip()}|\n")
+print (f"This is the right stripped version: |{person_name.title().rstrip()}|\n")
+print (f"This is the Full stripped version: |{person_name.title().strip()}|\n")
