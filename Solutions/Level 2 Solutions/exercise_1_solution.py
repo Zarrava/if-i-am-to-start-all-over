8254c4556ee995064162c5e727555c90991df58b
Primordialt <113912642+Primordialt@users.noreply.github.com>
2025-10-14 13:59:55 +0100

    level 2 exercise

---
+++ Solutions/Level 2 Solutions/exercise_1_solution.py
@@ -0,0 +1,4 @@
+print(5+3) #this line performs an addition operation and displays the result.
+print(10-2) #this line performs a subtraction operation and displays the result.
+print(4*2) #this line performs a multiplication operation and displays the result.
+print(16/2) #this line performs a division operation and displays the results
