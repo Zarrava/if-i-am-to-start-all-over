f4985ba86246115ca64c55f3d8f13d2a0cd6dabc
Primordialt <113912642+Primordialt@users.noreply.github.com>
2025-10-14 12:38:10 +0100

    Smart

---
+++ Solutions/exercise_12_solution.py
@@ -0,0 +1,6 @@
+famous_person = "Albert Einstein" #this stores the famous person name into the variable
+
+#this stores the quote and the famous person name into the variable message
+message = f'{famous_person.title()} once said, "A person who never made a mistake never tried anything new."'
+
+print (message) 
