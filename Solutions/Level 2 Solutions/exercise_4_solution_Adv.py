5ea2a2c64020a01b1958a2a79b34359d41612999
Primordialt <113912642+Primordialt@users.noreply.github.com>
2025-10-24 16:09:08 +0100

    solutions

--- Solutions/Level 2 Solutions/exercise_4_solution_Adv.py
+++ Solutions/Level 2 Solutions/exercise_4_solution_Adv.py
@@ -1,6 +1,8 @@
 #this is to create a list called names
 names = ["aderonke", "adekunle", "zeena", "eri", "ara", "elliott", "diva"]
 
+symb = "undefined" #this variable will be used to hold names so that we can carryout slice operations init
+
 #gets the user to input their first name
 user_name = input("Kindly enter your name: ").strip().lower()
 
@@ -20,10 +22,10 @@ elif user_name == names[3]:
     symb = names[3]
     print(f"{symb[0].upper()} for {names[3].title()}")
 elif user_name == names[4]:
-    symb == names[4]
+    symb == names[4] 
     print(f"{symb[0].upper()} for {names[4].title()}")
 elif user_name == names[5]:
-    symb == names[5]
+    symb == names[5] 
     print(f"{symb[0].upper()} for {names[5].title()}")
 elif user_name == names[6]:
     symb == names[6]
