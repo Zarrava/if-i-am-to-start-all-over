bfe2121fbdd278beb6e776f07ce58e9483ab5324
Primordialt <113912642+Primordialt@users.noreply.github.com>
2025-11-04 14:07:26 +0100

    comments added

--- Solutions/Level 2 Solutions/exercise_5_solution.py
+++ Solutions/Level 2 Solutions/exercise_5_solution.py
@@ -1,4 +1,4 @@
-#this is creating a variable that is containing a list of cars.
+#this is creating a variable that is containing a list of cars.f
 my_list = ["tesla", "benz", "toyota", "maybach", "ferari", "bugatti"]
 
 print (f"I love {my_list[0].title()} cars")
